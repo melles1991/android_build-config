68803c99ced77ed6286464f2eed280272fcb18e7
Galitsky Dmytro <melles.android@gmail.com>
2022-12-20 16:14:00 +0200

    Init build

---
+++ build.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+SECONDS=0 # builtin bash timer
+echo "--- Setup"
+cd $HOME/$HOME_DIR
+
+export OVERRIDE_TARGET_FLATTEN_APEX=true
+
+export TYPE=userdebug
+export DEVICE_START=onclite 
+export LOGBUILD="/tmp/android-build-${DEVICE_START}.log"
+
+echo "--- Clean"
+rm $HOME/$HOME_DIR/tmp/android-*.log || true
+rm $HOME/$HOME_DIR/out/target/product/$DEVICE_START/ExodusOS-*.zip || true
+
+# Telegram setup
+push_message() {
+    curl -s -X POST \
+        https://api.telegram.org/bot5579959772:AAHJ1cvfipl05kxYhNQBvLy7b60vGmeQSRE/sendMessage \
+        -d chat_id="-1001593139005" \
+        -d text="$1" \
+        -d "parse_mode=html" \
+        -d "disable_web_page_preview=true"
+}
+
+push_document() {
+    curl -s -X POST \
+        https://api.telegram.org/bot5579959772:AAHJ1cvfipl05kxYhNQBvLy7b60vGmeQSRE/sendDocument \
+        -F chat_id="-1001593139005" \
+        -F document=@"$1" \
+        -F caption="$2" \
+        -F "parse_mode=html" \
+        -F "disable_web_page_preview=true"
+}
+ 
+echo "--- Building"
+. build/envsetup.sh
+mka cleaninstall
+breakfast lineage_$DEVICE_START-$TYPE
+export OVERRIDE_TARGET_FLATTEN_APEX=true
+export BUILD_DATE=$(date '+%Y-%m-%d  %H:%M')
+# Push message if build started
+push_message "<b>Start building ExodusOS for <code>$DEVICE_START</code></b>
+<b>BuildDate:</b> <code>$BUILD_DATE</code>"
+brunch $DEVICE_START | tee $LOGBUILD
+
+rom="out/target/product/$DEVICE_START/ExodusOS-*.zip"
+if [ -f "$rom" ]; then
+	echo -e "$blue --- Uploading *.zip. $nocol"
+	scp out/target/product/$DEVICE_START/ExodusOS-*.zip melles1991@frs.sourceforge.net:/home/frs/project/exodusos/ExodusOS/onclite/$DEVICE_START/squirrel
+
+	# Push to telegram
+	push_message "ExodusOS for <code>$DEVICE_START</code>
+	<b>Type:</b> <code>$TYPE</code>
+	<b>BuildDate:</b> <code>$BUILD_DATE</code>
+	<b>md5 checksum :</b> <code>$(md5sum "$rom" | cut -d' ' -f1)</code>"
+
+	push_document "$LOGBUILD" "
+	<b>ExodusOS for <code>$DEVICE_START</code> compiled succesfully!</b>
+	Total build time <b>$((SECONDS / 60))</b> minute(s) and <b>$((SECONDS % 60))</b> second(s) !
+	#logs #$DEVICE_START "
+	echo -e "(i)          Send to telegram succesfully!\n"
+else
+	echo -e "$red \Rom Compilation failed! Fix the errors!\n $nocol"
+	# Push message if build error
+	push_message "$BUILDER! <b>Failed building ExodusOS for <code>$DEVICE_START</code></b> 
+	<b>Please fix it...!</b>
+	Total build time <b>$((SECONDS / 60))</b> minute(s) and <b>$((SECONDS % 60))</b> second(s) !"
+	push_document "$LOGBUILD" "#$DEVICE_START #error #exodus "
+fi
---
+++ sync.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+#
+# Compile script for kernel
+# Copyright (C) 2022 Craft Rom.
+
+SECONDS=0 # builtin bash timer
+
+#Set Color
+blue='\033[0;34m'
+grn='\033[0;32m'
+yellow='\033[0;33m'
+red='\033[0;31m'
+nocol='\033[0m'
+txtbld=$(tput bold)
+txtrst=$(tput sgr0) 
+
+export DATE=$(date '+%Y-%m-%d  %H:%M')
+export HOME_DIR="exodus"
+
+# Telegram setup
+push_message() {
+    curl -s -X POST \
+         https://api.telegram.org/bot5579959772:AAHJ1cvfipl05kxYhNQBvLy7b60vGmeQSRE/sendMessage \
+        -d chat_id="-1001695676652" \
+        -d text="$1" \
+        -d "parse_mode=html" \
+        -d "disable_web_page_preview=true"
+}
+
+push_message "<b>Build ExodusOS bot is running.</b>
+<b>Date:</b> <code>$DATE</code>"
+cd $HOME && sudo apt-get install git -y && git clone https://github.com/CraftRom/scripts && cd scripts && sudo bash setup/android_build_env.sh
+echo -e "$blue    \nDownloading manifest and initialized repo.\n $nocol"
+push_message "Downloading manifest and initialized repo"
+cd $HOME
+mkdir -p $HOME_DIR
+cd $HOME/$HOME_DIR
+repo init -u https://github.com/ExodusOS/android.git -b lineage-19.1
+repo sync --detach --current-branch --no-tags --force-remove-dirty --force-sync
