f5972d49e254601611ce7e186ab52f5ba933b10f
Galitsky Dmytro <melles.android@gmail.com>
2022-12-20 21:27:48 +0200

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -45,6 +45,8 @@ chmod a+rx ~/.bin/repo
 echo -e "$blue    \nDownloading manifest and initialized repo.\n $nocol"
 push_message "Downloading manifest and initialized repo"
 echo -e "$blue    \n initialized repo.\n $nocol"
+mkdir -p $SYNC_PATH/exodus
+cd $SYNC_PATH/exodus
 repo init --depth=1 -u $MANIFEST -b $MANIFEST_BRANCH
 echo -e "$blue    \n end initialized repo.\n $nocol"
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-remove-dirty --force-sync
