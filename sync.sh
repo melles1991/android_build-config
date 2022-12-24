0f908a40fdab12127ecad93e00ff2db3e733a0ee
Galitsky Dmytro <melles.android@gmail.com>
2022-12-24 22:24:39 +0200

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -16,13 +16,6 @@ txtrst=$(tput sgr0)
 
 export DATE=$(date '+%Y-%m-%d  %H:%M')
 
-# Init
-sudo apt-get install git-core gnupg flex bison build-essential zip curl zlib1g-dev gcc-multilib g++-multilib libc6-dev-i386 libncurses5 lib32ncurses5-dev x11proto-core-dev libx11-dev lib32z1-dev libgl1-mesa-dev libxml2-utils xsltproc unzip fontconfig
-mkdir -p ~/.bin
-PATH="${HOME}/.bin:${PATH}"
-curl https://storage.googleapis.com/git-repo-downloads/repo > ~/.bin/repo
-chmod a+rx ~/.bin/repo
-
 # Telegram setup
 push_message() {
     curl -s -X POST \
